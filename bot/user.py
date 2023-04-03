e08c52a6f6b82fdf0f4bf4d6971124049adfdaa5
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-04-04 03:13:32 +0700

    update user.py

--- bot/user.py
+++ bot/user.py
@@ -1,20 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) Shrimadhav U K
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-""" MtProto User """
 
 from ast import parse
 from pyrogram import (
@@ -22,10 +6,10 @@ from pyrogram import (
     __version__,
     enums
 )
-from bot import (
+from bot import LOGGER
+from config import (
     API_HASH,
     APP_ID,
-    LOGGER,
     TG_BOT_WORKERS
 )
 

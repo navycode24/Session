789c6c4fa310dba2ea5c2f82460c3d799b9ceb12
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-04-04 03:12:41 +0700

    update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,49 +1,12 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-""" credentials """
 
 import logging
 from dotenv import load_dotenv
 from logging.handlers import RotatingFileHandler
-from bot.get_config import get_config
-from .bot import Bot
-# apparently, no error appears even if the path does not exists
-load_dotenv("config.env")
-
-# The Telegram API things
-# Get these values from my.telegram.org or Telegram: @useTGxBot
-API_HASH = get_config("API_HASH", should_prompt=True)
-APP_ID = get_config("APP_ID", should_prompt=True)
-# get a token from @BotFather
-TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", should_prompt=True)
-
-# Number of update workers to use.
-# 4 is the recommended (and default) amount,
-# but your experience may vary.
-# Note that going crazy with more workers
-# wont necessarily speed up your bot,
-# given the amount of sql data accesses,
-# and the way python asynchronous calls work.
-TG_BOT_WORKERS = int(get_config("TG_BOT_WORKERS", "4"))
-#
-COMMM_AND_PRE_FIX = get_config("COMMM_AND_PRE_FIX", "/")
-# start command
-START_COMMAND = get_config("START_COMMAND", "start")
-# path to store LOG files
-LOG_FILE_ZZGEVC = get_config("LOG_FILE_ZZGEVC", "kynan.log")
+from pyrogram import (
+    Client,
+    __version__,
+    enums
+)
 
 
 logging.basicConfig(
@@ -67,47 +30,41 @@ def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
 
 
-# a dictionary to store the currently running processes
-AKTIFPERINTAH = {}
-# /start message when other users start your bot
-START_OTHER_USERS_TEXT = get_config(
-    "START_OTHER_USERS_TEXT",
-    (
-        """
-        Selamat Datang Di Kynan Session
-        """
-    )
+from config import (
+    API_HASH,
+    APP_ID,
+    TG_BOT_TOKEN,
+    TG_BOT_WORKERS,
 )
-INPUT_PHONE_NUMBER = get_config("INPUT_PHONE_NUMBER", (
-    "Masukkan Nomor Akun Telegram"
-))
-RECVD_PHONE_NUMBER_DBP = get_config("RECVD_PHONE_NUMBER_DBP", (
-    "Mencoba mengirim otp, silahkan cek otp"
-))
-ALREADY_REGISTERED_PHONE = get_config("ALREADY_REGISTERED_PHONE", (
-    "Periksa Pesan Masuk"
-))
-CONFIRM_SENT_VIA = get_config("CONFIRM_SENT_VIA", (
-    "kode otp dikirim dari {}"
-))
-RECVD_PHONE_CODE = get_config("RECVD_PHONE_CODE", (
-    "Mencoba mengirim otp, silahkan cek otp"
-))
-NOT_REGISTERED_PHONE = get_config("NOT_REGISTERED_PHONE", (
-    "Nomor terverifikasi belum terdaftar tele gan"
-))
-PHONE_CODE_IN_VALID_ERR_TEXT = get_config(
-    "Kode otp salah su. ketik atau tekan ini /start"
-)
-TFA_CODE_IN_VALID_ERR_TEXT = get_config(
-    "Kode verif salah su. ketik atau tekan ini /start"
-)
-ACC_PROK_WITH_TFA = get_config("ACC_PROK_WITH_TFA", (
-    "Diverif 2 langkah nih, paste dibawah cuy"
-))
-SESSION_GENERATED_USING = get_config("SESSION_GENERATED_USING", (
-    "Terima kasih telah menggunakan bot ini ..."
-))
 
+
+class Bot(Client):
+    """ modded client for SessionMakerBot """
+
+    def __init__(self):
+        super().__init__(
+            name="SessionMakerBot",
+            api_hash=API_HASH,
+            api_id=APP_ID,
+            bot_token=TG_BOT_TOKEN,
+            plugins={
+                "root": "bot/plugins"
+            },
+            workers=TG_BOT_WORKERS,
+            parse_mode=enums.ParseMode.HTML
+        )
+        self.LOGGER = LOGGER
+
+    async def start(self):
+        await super().start()
+        usr_bot_me = self.me
+        self.LOGGER(__name__).info(
+            f"@{usr_bot_me.username} based on Pyrogram v{__version__} "
+            "Try /start."
+        )
+
+    async def stop(self, *args):
+        await super().stop()
+        self.LOGGER(__name__).info("SessionMakerBot stopped. Bye.")
 
 app = Bot()

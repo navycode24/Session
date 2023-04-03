a0e31856978ba45202b9d834e7284721893d12fc
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-04-04 03:56:48 +0700

    update recvd_phone_nunber.py

--- bot/plugins/recvd_phone_nunber.py
+++ bot/plugins/recvd_phone_nunber.py
@@ -1,19 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) Shrimadhav U K
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 
 from pyrogram import (
     Client,
@@ -33,11 +17,9 @@ from bot.user import User
 
 
 @app.on_message(
-    filters.text &
-    filters.private,
-    group=1
+    filters.text & filters.private, group=1
 )
-async def recvd_ph_no_message(_, message: Message):
+async def recvd_ph_no_message(client, message):
     w_s_dict = AKTIFPERINTAH.get(message.chat.id)
     if not w_s_dict:
         return
@@ -46,7 +28,7 @@ async def recvd_ph_no_message(_, message: Message):
         return
     del w_s_dict["START"]
     status_message = await message.reply_text(
-        RECVD_PHONE_NUMBER_DBP
+        "Mohon periksa pesan masuk anda, dan masukkan kode yang ada dengan menggunakan spasi setiap kode\n Contoh : 1 2 3 4 5"
     )
     loical_ci = User()
     w_s_dict["PHONE_NUMBER"] = message.text
@@ -57,9 +39,9 @@ async def recvd_ph_no_message(_, message: Message):
     w_s_dict["USER_CLIENT"] = loical_ci
 
     status_message = await status_message.edit_text(
-        ALREADY_REGISTERED_PHONE + "\n\n" + CONFIRM_SENT_VIA.format(
-            w_s_dict["SENT_CODE_R"].type.value
-        )
+        ALREADY_REGISTERED_PHONE
+        + "\n\n"
+        + CONFIRM_SENT_VIA.format(w_s_dict["SENT_CODE_R"].type.value)
     )
     w_s_dict["MESSAGE"] = status_message
     AKTIFPERINTAH[message.chat.id] = w_s_dict

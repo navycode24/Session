40a420e3f20433a63c15e1b5e20b3b5e050341cf
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-04-04 04:27:06 +0700

    update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,28 +1,18 @@
-#  MIT License
-#
-#  Copyright (c) 2019-2020 Dan <https://github.com/delivrance>
-#
-#  Permission is hereby granted, free of charge, to any person
-#  obtaining a copy of this software and associated
-#  documentation files (the "Software"), to deal in the Software
-#  without restriction, including without limitation the rights
-#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#  copies of the Software, and to permit persons to whom the Software is
-#  furnished to do so, subject to the following conditions:
-#
-#  The above copyright notice and this permission notice shall be
-#  included in all copies or substantial portions of the Software.
-#
-#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-#  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-#  ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
-#  THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+from uvloop import install
+from bot import app, aiosession, event_loop
 
-""" copied from PyroGram Assistant """
 
-from bot import app
 
-app.run()
+
+async def main():
+      await app.start()
+      
+      await idle()
+      await aiosession.close()
+      await app.stop()
+      
+      
+if __name__ == "__main__":
+    LOGGER("Ubot").info("Starting  Ubot")
+    install()
+    event_loop.run_until_complete(main())

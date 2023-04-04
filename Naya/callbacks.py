187db5767416c7b97c2e3e82a3ae485c2c08b726
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-04-04 13:36:11 +0700

    update callbacks.py

--- Naya/callbacks.py
+++ Naya/callbacks.py
@@ -48,8 +48,20 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
     elif query.startswith("pyrogram") or query.startswith("telethon"):
         try:
             if query == "pyrogram":
-#              await callback_query.answer("Inget Ya Bangsat, Ini Buat Pyrogram V2 bukan V1", show_alert=True)
+                #await callback_query.answer("Please note that the new type of string sessions may not work in all bots, i.e, only the bots that have been updated to pyrogram v2 will work!", show_alert=True)
                 await generate_session(bot, callback_query.message)
+                """
+            # Maybe in future it'll come back.
+            elif query == "pyrogram1":
+                await callback_query.answer()
+                await generate_session(bot, callback_query.message, old_pyro=True)
+                """
+            elif query == "pyrogram_bot":
+                await callback_query.answer("Please note that this bot session will be of pyrogram v2", show_alert=True)
+                await generate_session(bot, callback_query.message, is_bot=True)
+            elif query == "telethon_bot":
+                await callback_query.answer()
+                await generate_session(bot, callback_query.message, telethon=True, is_bot=True)
             elif query == "telethon":
                 await callback_query.answer()
                 await generate_session(bot, callback_query.message, telethon=True)
@@ -57,19 +69,6 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
             print(traceback.format_exc())
             print(e)
             await callback_query.message.reply(ERROR_MESSAGE.format(str(e)))
-"""
-            if query == "pyrogram":
-                #await callback_query.answer("Please note that the new type of string sessions may not work in all bots, i.e, only the bots that have been updated to pyrogram v2 will work!", show_alert=True)
-                await generate_session(bot, callback_query.message)
-                
-            # Maybe in future it'll come back.
-            elif query == "pyrogram1":
-                await callback_query.answer()
-                await generate_session(bot, callback_query.message, old_pyro=True)
-            elif query == "telethon":
-                await callback_query.answer()
-                await generate_session(bot, callback_query.message, telethon=True)
-"""
 
 
 ERROR_MESSAGE = "Buset Eror Jink! \n\n**Error** : {} " \
